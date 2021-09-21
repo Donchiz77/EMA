0f561279521c56ef2bdc8b7c722dbe362df4379d
Zdeal <jedimastershake@gmail.com>
2021-09-20 22:10:48 -0400

    file restructure to put schedule in a component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/navbar/index.js";
-import {
-  Inject,
-  ScheduleComponent,
-  Day,
-  Week,
-  WorkWeek,
-  Month,
-  Agenda,
-} from "@syncfusion/ej2-react-schedule";
+import Schedule from "./components/pages/schedule/index";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <ScheduleComponent currentView="Month">
-        <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
-      </ScheduleComponent>
+      <Schedule />
     </div>
   );
 }
