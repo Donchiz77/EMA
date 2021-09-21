bbfe1e13ac7466373515fc555001d07339c53ed4
Zdeal <jedimastershake@gmail.com>
2021-09-20 21:02:41 -0400

    calendar started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
-import './App.css';
-import Navbar from "./components/navbar/index.js"
+import "./App.css";
+import Navbar from "./components/navbar/index.js";
+import {
+  Inject,
+  ScheduleComponent,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+} from "@syncfusion/ej2-react-schedule";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-     
+      <ScheduleComponent currentView="Month">
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+      </ScheduleComponent>
     </div>
   );
 }
