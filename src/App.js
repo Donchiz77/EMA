24f727e7a44b4c608bf24b8d6a3dbfe9eafadeac
Aldaine Knight <stevennite1@gmail.com>
2021-09-24 16:59:01 -0400

    updates to components

--- src/App.js
+++ src/App.js
@@ -1,1 +1,33 @@
 
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link, Redirect, Switch } from "react-router-dom";
+import NavTab from "./components/Navbar";
+import Home from "./components/pages/homePage";
+import patientDemographic from "./components/pages/patientDemographic/index";
+import PatientRecord from "./components/pages/patientRecord/index";
+import Schedule from "./components/pages/schedule/index";
+import Wrapper from "./components/Wrapper";
+
+
+
+
+function App () {
+  return (
+    <Router>
+      <div>
+        <NavTab />
+        <Wrapper>
+        <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/PatientDemographic" component={patientDemographic} />
+        <Route exact path="/patientRecord" component={PatientRecord} />
+        <Route path="/schedule" component={Schedule} />
+        </Switch>
+        </Wrapper>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
