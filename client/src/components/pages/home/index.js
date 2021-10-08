15264e4724d8e472ecef5ce46b8053d82400c46d
Aldaine Knight <stevennite1@gmail.com>
2021-10-08 09:12:41 -0400

    commit code for cards

--- client/src/components/pages/home/index.js
+++ client/src/components/pages/home/index.js
@@ -3,32 +3,40 @@ import React from "react";
 import classes from '../home/home.css';
 import "../home/home.css";
 import logo from '../../../assets/images/Moar.png';
+import img1 from '../../../assets/images/doctor-image-1.jpg';
 
 
 
-<<<<<<< HEAD
-
-function home(){
-   
-=======
 function home () {
   return(
-    <div className={classes.Photo}>
-      <img className={classes.Logo} src={logo} alt="logo"></img>
-    </div>
+    <section>
+    
+          <div className={classes.Photo}>
+            <img className={classes.Logo} src={logo} alt="logo"></img>
+          </div>
+
+          <div className= "card text-center" >
+            <div className="overflow">
+                <img src={img1} alt="doctor"/>
+            </div>
+            <div className="card-body text-dark">
+              <h4 className="card-title">Card Title</h4>
+              <p className="card-text text-secondary">
+                Lorem Ipsum is simply dummy text of 
+                the printing and typesetting industry. 
+                Lorem Ipsum has been the industry
+              </p>
+              <a href="patientrecord" className= "btn btn-outline-success">Go Anywhere</a>
+            </div>
+          </div>
+
+    </section>
   )
 }
->>>>>>> 2f3b972872cc7ffcf001313ffb68e37828b937fa
 
 
-    return (
-      <div>
-        <h1>Home</h1>
-      
-        
-      </div>
-    );
-  }
+   
+  
 
 
 
