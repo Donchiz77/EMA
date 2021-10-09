571f0a7135fb4ab27644a7eb023f9560c03a245a
Zdeal <jedimastershake@gmail.com>
2021-10-08 20:25:30 -0400

    home landing corrected

--- client/src/App.js
+++ client/src/App.js
@@ -7,40 +7,26 @@ import Home from "./components/pages/home/index";
 import LoginScreen from "./components/screens/Login";
 import RegisterScreen from "./components/screens/Register";
 import ForgotPasswordScreen from "./components/screens/ForgotPassword";
-import ResetPasswordScreen from "./components/screens/ResetPassword";
+import ResetPassword from "./components/screens/ResetPassword";
 import React from "react";
 import Schedule from "./components/pages/schedule/index";
 import PatientRecord from "./components/pages/patientRecord/index";
-//import DefHeader from "./components/pages/definition/defHeader";
-////import Definition from "./components/pages/definition/definition";
-//import { Container } from "@material-ui/core";
 
 const App = () => {
   return (
-    //<div
-    //  className="App"
-    //  style={{ height: "100vh", backgroundColor: "gray", color: "white" }}
-    //>
-    //  <Container
-    //    maxWidth="md"
-    //    style={{ display: "flex", flexDirection: "column", height: "100vh" }}
-    //  >
-    //    <DefHeader category={category} setCategory={setCategory} />
-    //  </Container>
-    //</div>
     <Router>
       <div className="app">
         <Switch>
+          <PrivateRoute exact path="/" component={Navbar} />
+          <Route exact path="/login" component={LoginScreen} />
+          <Route exact path="/register" component={RegisterScreen} />
+          <PrivateRoute exact path="/home" components={<Home />} />
           <PrivateRoute
             exact
             path="/patientRecord"
             components={<PatientRecord />}
           />
-          <PrivateRoute exact path="/home" components={<Home />} />
           <PrivateRoute exact path="/schedule" components={<Schedule />} />
-          <Route exact path="/" component={Navbar} />
-          <Route exact path="/login" component={LoginScreen} />
-          <Route exact path="/register" component={RegisterScreen} />
           <Route
             exact
             path="/forgotpassword"
@@ -49,7 +35,7 @@ const App = () => {
           <Route
             exact
             path="/passwordreset/:resetToken"
-            component={ResetPasswordScreen}
+            component={ResetPassword}
           />
         </Switch>
       </div>
--- client/src/components/pages/home/index.js
+++ client/src/components/pages/home/index.js
@@ -1,21 +1,14 @@
 import React from "react";
-import classes from '../home/home.css';
+import classes from "../home/home.css";
 import "../home/home.css";
-import logo from '../../../assets/images/Moar.png';
+import logo from "../../../assets/images/Moar.png";
 
-
-
-function home () {
-  return(
+function home() {
+  return (
     <div className={classes.Photo}>
       <img className={classes.Logo} src={logo} alt="logo"></img>
     </div>
-  )
+  );
 }
-
-
-
-
-
 
 export default home;
