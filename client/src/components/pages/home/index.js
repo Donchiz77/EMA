8b12510efe79130b03b771ed37fa64f6251572bd
Devin Pawline <devinpawline@outlook.com>
2021-10-08 22:52:59 -0400

    chagnes to home page

--- client/src/components/pages/home/index.js
+++ client/src/components/pages/home/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import "./home.css";
 import logo from '../../../assets/images/Moar.png';
-import Card from './Cards';
+
 
 
 
@@ -30,71 +30,8 @@ function home () {
                 <h3>Commitment to Community</h3>
                 <p>M.O.A.R mission is to transform the health of the community, one person at a time With offices in Charllote, Mathews, Pinville, our dedicated
                 team of doctors, social service workers and customer service representative are commited to improving health in North Carolina</p>
-          </div>
-          
-          <div >
-              <Card/>
-          </div>
-          
+          </div> 
   </div>
-  <footer className="footer container-fluid">
-      <div className="footer-middle">
-      <div className="container footer-container">
-        <div className="row">
-        {/*column*/}
-            <div className="col-md-3 col-sm-6 footer-col">
-              <h4>location</h4>
-                  <ul className="list-unstyled">
-                      <li><a href="something">mathews</a></li>
-                       <li><a href="something">Pinville</a></li>
-                        <li><a href="something">cornelia</a></li>
-                         <li><a href="something">rock hill</a></li>
-                  </ul>
-            </div>
-
-            <div className="col-md-3 col-sm-6 footer-col">
-              <h4>services</h4>
-                  <ul className="list-unstyled">
-                      <li><a href="something">faq</a></li>
-                       <li><a href="something">all the drugs you need</a></li>
-                        <li><a href="something">support</a></li>
-                
-                  </ul>
-            </div>
-
-            <div className="col-md-3 col-sm-6 footer-col ">
-              <h4>contact</h4>
-                  <ul className="list-unstyled">
-                      <li><a href="something">email us</a></li>
-                       <li><a href="something">call us</a></li>
-                        
-                  </ul>
-            </div>
-
-            <div className="col-md-3 col-sm-6 footer-col ">
-              <h4>follow us</h4>
-                  <ul className="list-unstyled">
-                      <li><a href="something"><i class="fab fa-facebook"></i></a></li>
-                       <li><a href="something"><i class="fab fa-twitter"></i></a></li>
-                        <li><a href="something"><i class="fab fa-instagram-square"></i></a></li>
-                  </ul>
-            </div>
-        </div>
-        {/*footer bottom*/}
-        <div className="footer-bottom">
-        <p className="text-xs-center">
-        &copy;{new Date().getFullYear()}  M.O.A.R all rights reserved
-        </p>
-      </div>
-      </div>
-      </div>       
-            
-  
-  </footer>
-  
-  
-
-  
 </section>
 </div>
   ) 
